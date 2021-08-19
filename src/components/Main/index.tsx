e90df933f7bfa42955171ad17ce8118d021c8c9d
Brenno Henrique <brennovicentini@DESKTOP-N8G6UTQ.localdomain>
2021-08-19 17:02:25 +0100

    Addstyled-components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,16 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Imagem de um atomo e Brenno devops" />
+    <S.Title>Brenno Devops</S.Title>
+    <S.Description>TypeScript, React, NextJS e styled Components</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um homem sentado programando"
+    />
+    ;
+  </S.Wrapper>
 )
 
 export default Main
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -7,9 +7,15 @@ describe('<Main />', () => {
     const { container } = render(<Main />)
 
     expect(
-      screen.getByRole('heading', { name: /react avançado/i })
+      screen.getByRole('heading', { name: /brenno devops/i })
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
